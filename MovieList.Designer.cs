c213c456c87fa0fd37e22e0ab77bc00f0ccb8b48
laday <laday@crimson.ua.edu>
2015-11-28 19:55:22 -0600

    Still playing with MovieList just adding it before y'all add something and i mess everything up again. PS RMFTR!!!!!!!!!

--- MovieList.Designer.cs
+++ MovieList.Designer.cs
@@ -28,11 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.wishList_BTN = new System.Windows.Forms.Button();
+            this.checkOut_BTN = new System.Windows.Forms.Button();
+            this.returnTo_BOX = new System.Windows.Forms.ComboBox();
+            this.sortBy_BOX = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 54);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(549, 212);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // wishList_BTN
+            // 
+            this.wishList_BTN.Location = new System.Drawing.Point(13, 309);
+            this.wishList_BTN.Name = "wishList_BTN";
+            this.wishList_BTN.Size = new System.Drawing.Size(158, 77);
+            this.wishList_BTN.TabIndex = 1;
+            this.wishList_BTN.Text = "Add to Wish List";
+            this.wishList_BTN.UseVisualStyleBackColor = true;
+            // 
+            // checkOut_BTN
+            // 
+            this.checkOut_BTN.Location = new System.Drawing.Point(226, 309);
+            this.checkOut_BTN.Name = "checkOut_BTN";
+            this.checkOut_BTN.Size = new System.Drawing.Size(158, 77);
+            this.checkOut_BTN.TabIndex = 2;
+            this.checkOut_BTN.Text = "Rent or Buy Movie";
+            this.checkOut_BTN.UseVisualStyleBackColor = true;
+            // 
+            // returnTo_BOX
+            // 
+            this.returnTo_BOX.FormattingEnabled = true;
+            this.returnTo_BOX.Items.AddRange(new object[] {
+            "Home Page",
+            "Wish List",
+            "Account Information"});
+            this.returnTo_BOX.Location = new System.Drawing.Point(440, 309);
+            this.returnTo_BOX.Name = "returnTo_BOX";
+            this.returnTo_BOX.Size = new System.Drawing.Size(121, 24);
+            this.returnTo_BOX.TabIndex = 3;
+            this.returnTo_BOX.Text = "Return To:";
+            // 
+            // sortBy_BOX
+            // 
+            this.sortBy_BOX.FormattingEnabled = true;
+            this.sortBy_BOX.Items.AddRange(new object[] {
+            "Genre",
+            "Alphabetical"});
+            this.sortBy_BOX.Location = new System.Drawing.Point(440, 24);
+            this.sortBy_BOX.Name = "sortBy_BOX";
+            this.sortBy_BOX.Size = new System.Drawing.Size(121, 24);
+            this.sortBy_BOX.TabIndex = 4;
+            this.sortBy_BOX.Text = "Sort By:";
+            // 
+            // MovieList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 423);
+            this.Controls.Add(this.sortBy_BOX);
+            this.Controls.Add(this.returnTo_BOX);
+            this.Controls.Add(this.checkOut_BTN);
+            this.Controls.Add(this.wishList_BTN);
+            this.Controls.Add(this.listBox1);
+            this.Name = "MovieList";
             this.Text = "MovieList";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button wishList_BTN;
+        private System.Windows.Forms.Button checkOut_BTN;
+        private System.Windows.Forms.ComboBox returnTo_BOX;
+        private System.Windows.Forms.ComboBox sortBy_BOX;
     }
 }
--- MovieList.cs
+++ MovieList.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace MIS220GroupProject
 {
@@ -15,6 +17,39 @@ namespace MIS220GroupProject
         public MovieList()
         {
             InitializeComponent();
+        }
+
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        { 
+        string dbStr = "Data Source = mis220.eil-server.cba.ua.edu; Initial Catalog = MovieRental; user id =uamis; password=RollTide";
+        string sqlIns = "SELECT Title, Genre, ReleaseDate, Rating FROM dbo.Movie WHERE title = '" + listBox1.Text "     ';" ;
+        SqlConnection conDataBase = new SqlConnection(dbStr);
+        SqlCommand cmdIns = new SqlCommand(sqlIns, conDataBase);
+        SqlDataReader myReader;
+
+            try
+            {
+                conDataBase.Open();
+                myReader = cmdIns.ExecuteReader();
+
+                while(myReader.Read())
+                    {
+                    string mTitle = myReader.GetString("Title");
+                    listBox1.Items.Add(mTitle);
+                    string mGenre = myReader.GetString("Genre");
+                    listBox1.Items.Add(mGenre);
+                    string mReleaseDate = myReader.GetString("ReleaseDate");
+                    listBox1.Items.Add(mReleaseDate);
+                    string mRating = myReader.GetString("Rating");
+                    listBox1.Items.Add(mRating);
+                }
+
+}
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
