4a1b55b3cd6979df054ffd59c445dd58a4fe0757
cagetsinger <cagetsinger@crimson.ua.edu>
2015-11-30 13:18:31 -0600

    commented out movielist.cs method because it is throwing errors

--- MovieList.cs
+++ MovieList.cs
@@ -21,35 +21,35 @@ namespace MIS220GroupProject
 
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
-        { 
-        string dbStr = "Data Source = mis220.eil-server.cba.ua.edu; Initial Catalog = MovieRental; user id =uamis; password=RollTide";
-        string sqlIns = "SELECT Title, Genre, ReleaseDate, Rating FROM dbo.Movie WHERE title = '" + listBox1.Text "     ';" ;
-        SqlConnection conDataBase = new SqlConnection(dbStr);
-        SqlCommand cmdIns = new SqlCommand(sqlIns, conDataBase);
-        SqlDataReader myReader;
+        {
+        //    string dbStr = "Data Source = mis220.eil-server.cba.ua.edu; Initial Catalog = MovieRental; user id =uamis; password=RollTide";
+        //    string sqlIns = "SELECT Title, Genre, ReleaseDate, Rating FROM dbo.Movie WHERE title = '" + listBox1.Text + "';";
+        //    SqlConnection conDataBase = new SqlConnection(dbStr);
+        //    SqlCommand cmdIns = new SqlCommand(sqlIns, conDataBase);
+        //    SqlDataReader myReader;
 
-            try
-            {
-                conDataBase.Open();
-                myReader = cmdIns.ExecuteReader();
+        //    try
+        //    {
+        //        conDataBase.Open();
+        //        myReader = cmdIns.ExecuteReader();
 
-                while(myReader.Read())
-                    {
-                    string mTitle = myReader.GetString("Title");
-                    listBox1.Items.Add(mTitle);
-                    string mGenre = myReader.GetString("Genre");
-                    listBox1.Items.Add(mGenre);
-                    string mReleaseDate = myReader.GetString("ReleaseDate");
-                    listBox1.Items.Add(mReleaseDate);
-                    string mRating = myReader.GetString("Rating");
-                    listBox1.Items.Add(mRating);
-                }
+        //        while (myReader.Read())
+        //        {
+        //            string mTitle = myReader.GetString("Title");
+        //            listBox1.Items.Add(mTitle);
+        //            string mGenre = myReader.GetString("Genre");
+        //            listBox1.Items.Add(mGenre);
+        //            string mReleaseDate = myReader.GetString("ReleaseDate");
+        //            listBox1.Items.Add(mReleaseDate);
+        //            string mRating = myReader.GetString("Rating");
+        //            listBox1.Items.Add(mRating);
+        //        }
 
-}
-            catch(Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show(ex.Message);
+        //    }
         }
     }
 }
