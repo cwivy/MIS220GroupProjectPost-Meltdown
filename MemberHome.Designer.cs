2bdddefe6ea8b15260d221b0c7d940aca96e3ec9
Carlton Ivy <cwivy@crimson.ua.edu>
2015-12-01 15:10:09 -0600

    Added logo picture and other things

--- MemberHome.Designer.cs
+++ MemberHome.Designer.cs
@@ -53,17 +53,17 @@
             this.accBalBox.Cursor = System.Windows.Forms.Cursors.Default;
             this.accBalBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.accBalBox.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.accBalBox.Location = new System.Drawing.Point(296, 25);
+            this.accBalBox.Location = new System.Drawing.Point(297, 36);
             this.accBalBox.Name = "accBalBox";
             this.accBalBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
-            this.accBalBox.Size = new System.Drawing.Size(149, 76);
+            this.accBalBox.Size = new System.Drawing.Size(169, 60);
             this.accBalBox.TabIndex = 1;
             this.accBalBox.Text = "25.00";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(296, 6);
+            this.label2.Location = new System.Drawing.Point(297, 17);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(89, 13);
             this.label2.TabIndex = 2;
@@ -133,7 +133,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(501, 264);
+            this.ClientSize = new System.Drawing.Size(494, 265);
             this.Controls.Add(this.memNameBox);
             this.Controls.Add(this.toRewardScreen_BTN);
             this.Controls.Add(this.toPaymentScreen_BTN);
